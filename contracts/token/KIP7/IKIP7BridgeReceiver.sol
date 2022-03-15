396b840002b3f187bf3c6508807261881cd28158
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-03-15 16:43:21 +0900

    [KIP7] Added requestValueTransfer function

--- contracts/token/KIP7/IKIP7BridgeReceiver.sol
+++ contracts/token/KIP7/IKIP7BridgeReceiver.sol
@@ -16,20 +16,6 @@
 
 pragma solidity 0.5.6;
 
-import "./KIP7Mintable.sol";
-import "./KIP7Burnable.sol";
-import "./KIP7Pausable.sol";
-import "./KIP7Metadata.sol";
-import "./KIP7ServiceChain.sol";
-
-contract ServiceChainTokenKIP7 is KIP7, KIP7Mintable, KIP7Burnable, KIP7Pausable, KIP7Metadata, KIP7ServiceChain {
-    string public constant NAME = "KIP7ServiceChain";
-    string public constant SYMBOL = "KIP7SCT";
-    uint8 public constant DECIMALS = 18;
-
-    uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(DECIMALS));
-
-    constructor(address _bridge) KIP7Metadata(NAME, SYMBOL, DECIMALS) KIP7ServiceChain(_bridge) public {
-        _mint(msg.sender, INITIAL_SUPPLY);
-    }
+contract IKIP7BridgeReceiver {
+    function onKIP7Received(address _from, address _to, uint256 _amount, uint256 _feeLimit, bytes memory _extraData) public;
 }
